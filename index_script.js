34aa953e09f364691582cf31b4547c84db58c55f
max <maxpavlov9991@gmail.com>
2019-04-13 21:28:48 +0300

    fixed some bugs of last commit

--- index_script.js
+++ index_script.js
@@ -1,5 +1,10 @@
 'use strict';
 
+function slowScroll(id) {
+    $('html').animate({
+        scrollTop: $(id).offset().top
+    }, 500);
+}
 $(document).on("scroll", function (){
     if($(window).scrollTop() === 0){
         $("header").removeClass("fixed");
